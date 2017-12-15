c3da7802c1f618ad07c9843efb08a91cc3671694
Sharona Young <youngs_e@hotmail.com>
2017-12-15 16:14:36 -0500

    added psuedo code

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,13 @@
+// Giphy API Key: CO5Bdmd3pzct7qe3fGGS3tdGFF1biYxa
+
+// code to make script run once page is ready
+// code to prevent defaults from happening
+// variable to hold array of topics
+// code to create button from items from the  topic array, use a for loop to append topics to button
+// on click function to get information from GIPHY (10 gifs). Display in html, not animated
+// logic to animate the gif. goes back still when clicked again
+// each gif will display its rating
+// dynamically create a text input box for user to add to topics array, a new button will be created
+
+
+
