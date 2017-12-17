53c491f9b3a33604115bc764f2e6df8d2e741465
Sharona Young <youngs_e@hotmail.com>
2017-12-17 17:36:54 -0500

    js file updates to buttons

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,36 +2,24 @@
 
 // code to make script run once page is ready
 $(document).ready(function() {
-
-// variable to hold array of topics
-var topics = ["Orlando", "Las Vegas", "San Francisco"];
-console.log(topics)
-
-// Function that holding location gifs
-function locationDisplay () {
-
-  var topic = $(this).attr("locations");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=CO5Bdmd3pzct7qe3fGGS3tdGFF1biYxa";
-
-  // AJAX GET request
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }) .done(function(response) {
-    $("#location-view").text(JSON.stringify(response));
-    renderLocation();
-  });
-}
   
-  // function to display gifs
-  function renderLocation() {
+  // variable to hold array of topics
+  var topics = ["Orlando", "Las Vegas", "San Francisco"];
+  console.log(topics)
 
+  // Function to request giphy images to display when any location button is clicked
+  function displayLocationGif () {
+    
+  }
+  // Function that will display location buttons
+  function locationDisplay () {
+  
     // the location buttons will need to be remove before a new button is added, to avoid repetitive buttons
     $("#results").empty();
-
+  
     // for loop to go thru topics array
     for (var i = 0; i < topics.length; i++) {
-
+  
     // create buttons from items from the topic array, use a for loop to append topics to button
     // generate buttons dynamically for each location
     var a = $("<button>");
@@ -43,35 +31,26 @@ function locationDisplay () {
     a.text(topics[i]);
     // create a new button in the HTML
     $("#results").append(a);
+    }
   }
-}
-
-// on click event for the travel here button
-$("#add-location").click(function(event) {
-
-// prevent code to keep the form from reloading when button is clicked.
-event.preventDefault();
-
-// get the text from the input box, user entered
-var topic = $("#new-location-input").val().trim();
-
-// the location the user entered is added to topic array
-topics.push(topic);
-
-// call the function created to display gif
-locationDisplay();
-});
-
-$(document).click(".topic", locationDisplay);
-
-// call locationDisplay function to display initial list of locations
-// locationDisplay();
-});
-
-// on click function to get information from GIPHY (10 gifs). Display in html, not animated
-// logic to animate the gif. goes back still when clicked again
-// each gif will display its rating
-// dynamically create a text input box for user to add to topics array, a new button will be created
-
-
-
+  
+  // on click event for the travel here button
+  $("#add-location").click(function(event) {
+  
+  // prevent code to keep the form from reloading when button is clicked.
+  event.preventDefault();
+  
+  // get the text from the input box, user entered
+  var topic = $("#new-location-input").val().trim();
+  
+  // the location the user entered is added to topic array
+  topics.push(topic);
+  
+  // call the function created to display gif
+  locationDisplay();
+  });
+  
+  // call locationDisplay function to display initial list of locations
+  locationDisplay();
+  
+  });
